f9afd9f7d17962a92f5b4d670a402f9f6aad87f8
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-06-07 18:10:59 +0100

    Add Logic to generate matching and spot palette color

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import './styles/main.scss';
 import Color from 'color';
 // ---
 const color = document.querySelector('#color');
-const square = document.querySelector('#square')
-// const gradient = document.querySelector('.gradient')
+// const circle = document.querySelector('#circle')
+// const spot = document.querySelector('.spot')
+
+const squares = [...document.querySelectorAll('.square')]
 const gradients = [...document.querySelectorAll('.gradient')]
+const matchings = [...document.querySelectorAll('.matching')]
+const palettes = [...document.querySelectorAll('.palette')]
 
 const applyColor = (gradient, color, degree) => {
   gradient.style.backgroundColor = color.rotate(degree).hsl().string()
@@ -12,16 +16,27 @@ const applyColor = (gradient, color, degree) => {
 
 color.addEventListener('change', event => {
   const color = Color(event.target.value)
-  square.style.backgroundColor = event.target.value;
-  // gradient.style.backgroundColor = color.rotate(40).hsl().string()
+
+  squares.forEach(square => { 
+    square.style.backgroundColor = event.target.value;
+  })
+  // circle.style.backgroundColor = event.target.value;
+  // spot.style.backgroundColor = event.target.value
 
   gradients.forEach((gradient, index) => {
 
-    applyColor(gradient, color, (index + 1) * 40)
+    applyColor(gradient, color, (index + 1) * 76)
+  })
+
+  matchings.forEach((matching, index) => {
+    applyColor(matching, color, (index + 1) * 87)
+  })
+
+  palettes.forEach((spot, index) => {
+    applyColor(spot, color, (index + 1) * 63)
   })
 })
 
 
-// square.addEventListener('click', e => {
-//   console.log(e.target)
-// })
+// Matching Gradient //
+
